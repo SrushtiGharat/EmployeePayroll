1ab66213d3c5eae63d4ba8eae622a69edf3e757a
Srushti <Srushti@DESKTOP-QP0QMA4>
2020-11-01 23:19:04 +0530

    [Srushti]Add.Create employee payroll table

--- EmployeePayrollQuery.sql
+++ EmployeePayrollQuery.sql
@@ -2,3 +2,14 @@
 create database payroll_service
 select * from sys.databases where name = 'payroll_service'
 use payroll_service
+
+--Create table--
+create table employee_payroll
+(
+id int identity(1,1) not null,
+name varchar(25) not null,
+salary money not null,
+start_date date not null
+)
+
+select * from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'employee_payroll'
